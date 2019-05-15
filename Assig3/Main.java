5cc416cfa673bc8511c888e56aad6ae375878ade
Andrew Bell <majacow@gmail.com>
2019-05-14 18:57:54 -0700

    made files for starting to work on

---
+++ Assig3/Main.java
@@ -0,0 +1,18 @@
+/** Team SAGA */
+/** Card Deck */
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      //set up scanner object
+      Scanner keyboard = new Scanner(System.in);
+
+      System.out.print("Please enter your first name: ");
+      String firstName = keyboard.next();
+
+      System.out.println(firstName);
+   }
+}
