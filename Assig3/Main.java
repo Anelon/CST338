101f289531e136abd80d1fe8f060a67dc538de65
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2019-05-15 13:01:18 -0700

    Implemented Deck class, added stubs in Card class

--- Assig3/Main.java
+++ Assig3/Main.java
@@ -2,18 +2,27 @@
 /** Card Deck */
 import java.util.Scanner;
 import java.text.DecimalFormat;
-
+import java.util.Random;
 class Card
 {
-   enum Suit {spade, heart, club, diamond};
+   public enum Suit {spades, hearts, clubs, diamonds};
    private char value;
    private Suit suit;
 
    //default constructor
-   public Card() 
+   public Card()
    {
       value = '0';
-      suit = Suit.spade;
+      suit = Suit.spades;
+   }
+
+   public Card(char cardNumber, Suit suit)
+   {
+
+   }
+   public Card(Card copy)
+   {
+
    }
 
    //card to string converter
@@ -23,14 +32,168 @@ class Card
    }
 }
 
+
+
+
+
+
+
+class Deck
+{
+
+   public Deck(int numPacks)
+   {
+      allocateMasterPack();
+      init(numPacks);
+   }
+
+
+
+   public Deck()
+   {
+      this(1);
+   }
+
+
+
+   public void init(int numPacks)
+   {
+
+      int totalCardIndex = 0;
+      for (int packNumber = 0; packNumber < numPacks; ++packNumber)
+      {
+         for (int masterIndex = 0; masterIndex < masterPack.length; ++masterIndex)
+         {
+            cards[totalCardIndex] = new Card(masterPack[masterIndex]);
+               ++totalCardIndex;
+         }
+      }
+   }
+
+
+
+   public void shuffle()
+   {
+      Random randomGenerator = new Random();
+      int randomIndex = randomGenerator.nextInt(cards.length);
+      for (int i = 0; i < cards.length; ++i)
+      {
+         Card placeholder = new Card(cards[randomIndex]);
+         cards[randomIndex] = cards[i];
+         cards[i] = placeholder;
+         randomIndex = randomGenerator.nextInt(cards.length);
+      }
+   }
+
+
+
+   public Card dealCard()
+   {
+      return new Card(cards[topCard++]);
+   }
+
+
+   public Card inspectCard(int k)
+   {
+      Card cardToInspect;
+
+      if (k < cards.length && k >= 0)
+      {
+         cardToInspect = new Card(cards[k]);
+      }
+      else
+         cardToInspect  = new Card('0', Card.Suit.spades);
+
+      return cardToInspect;
+   }
+
+
+   public Card getTopCard()
+   {
+      return new Card(cards[topCard]);
+   }
+
+
+   /**
+    * The following function converts int values to a Card.Suit enum.
+    * For use with quick access of enums.
+    *
+    * @param toBeConverted - integer to be converted to a Card.Suit enum
+    * @return one of the 4 suits as defined in Card class. 0 = hearts,
+    * 1 = clubs, 2 = diamonds, all other ints will return spades
+    */
+   private static Card.Suit intToSuit(int toBeConverted)
+   {
+      switch (toBeConverted)
+      {
+         case 0:
+            return Card.Suit.hearts;
+         case 1:
+            return Card.Suit.clubs;
+         case 2:
+            return Card.Suit.diamonds;
+         default:
+            return Card.Suit.spades;
+      }
+
+   }
+
+
+   private static void allocateMasterPack()
+   {
+      if (masterPack[0] == null)
+      {
+         int masterIndex = 0;
+
+         for(int suitInt = 0; suitInt < 4; ++suitInt)
+         {
+            for (int cardNum = 1; cardNum <=13; ++cardNum)
+            {
+               char cardValue = (char)cardNum;
+               switch (cardNum) {
+                  case 1: {
+                     cardValue = 'A';
+                     break;
+                  }
+                  case 10: {
+                     cardValue = 'T';
+                     break;
+                  }
+                  case 11: {
+                     cardValue = 'J';
+                     break;
+                  }
+                  case 12: {
+                     cardValue = 'Q';
+                     break;
+                  }
+                  case 13: {
+                     cardValue = 'K';
+                     break;
+                  }
+               }
+               masterPack[masterIndex] = new Card(cardValue, intToSuit(suitInt));
+               ++masterIndex;
+            }
+         }
+      }
+   }
+
+   private Card[] cards;
+   private int topCard;
+   public final int MAX_CARDS = 6*52;
+   private static Card[] masterPack = new Card[52];
+
+}
+
 class Hand
 {
-   //need to change the ammount of cards (or use an arrayList?)
+   //need to change the amount of cards (or use an arrayList?)
    private static Card[] myArray = new Card[120];
    private static int numCards;
 
    //default constructor
-   public Hand() 
+   public Hand()
    {
       numCards = -1;
    }
