fe009fef0f14ca609bb3f66846133036369c6255
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2019-05-18 13:07:11 -0700

    Added documentation to Deck class

--- Assig3/Main.java
+++ Assig3/Main.java
@@ -52,8 +52,8 @@ class Card
    private boolean isValid(char newValue, Suit newSuit)
    {
       if ((newValue >= MIN_VALUE && newValue <= MAX_VALUE)
-            || newValue == ACE || newValue == KING || newValue == QUEEN
-            || newValue == TEN || newValue == JACK)
+         || newValue == ACE || newValue == KING || newValue == QUEEN
+         || newValue == TEN || newValue == JACK)
       {
          return true;
       }
@@ -133,13 +133,32 @@ class Card
 
 
 
-
+/**
+ * Class containing data for a deck of cards.  Contains static information of
+ * all possible cards as well as current in-use cards/order within the Deck object.
+ * 
+ * public static int MAX_CARDS - Maximum number of cards allowed in play at once.
+ * private Card[] cards - current "in use" cards for playing
+ * private int topCard - top card of the deck, next to be "dealt".
+ * private static Card[] masterPack - static representation of all possible cards
+ */
 class Deck
 {
 
+
+   
+   
+   
+   
+   /**
+    * Constructor that creates the deck based on the number of "packs" given.
+    * Note that only a maximum of MAX_CARDS cards can be used.
+    * 
+    * @param numPacks - number of traditional 52 card packs to be used in deck creation
+    */
    public Deck(int numPacks)
    {
-	  
+
       allocateMasterPack();
       if (52*numPacks > MAX_CARDS)
          cards = new Card[MAX_CARDS];
@@ -148,15 +167,31 @@ class Deck
       init(numPacks);
    }
 
-
-
+   
+   
+   
+   
+   
+   /**
+    * Empty constructor. Initializes the deck with a default of a single 52 card pack.
+    */
    public Deck()
    {
       this(1);
    }
 
 
-
+   
+   
+   
+   
+   /**
+    * initializes the playable cards in cards[] based on the number of packs.
+    * The resulting cards[] object will contain ((int)numPacks) copies of the masterPack.
+    * Called by the constructors.
+    * 
+    * @param numPacks number of 52 card packs to be used
+    */
    public void init(int numPacks)
    {
 
@@ -172,7 +207,14 @@ class Deck
    }
 
 
-
+   
+   
+   
+   
+   /**
+    * Randomizes the cards within the in-use cards[] object and resets the topCard to 
+    * the first position.
+    */
    public void shuffle()
    {
       Random randomGenerator = new Random();
@@ -188,13 +230,26 @@ class Deck
    }
 
 
-
+   /**
+    * "Deals" the top card from the deck. The next card in cards[] becomes the new
+    * topCard.
+    * 
+    * @return the topCard at the time of calling
+    */
    public Card dealCard()
    {
       return new Card(cards[topCard++]);
    }
 
 
+   /**
+    * Gives information from the card at the given index within cards[].
+    * Note that the card at the index is not returned, only a copy in order
+    * to prevent the card itself from being altered in any way.
+    * 
+    * @param k - index of cards[] to be checked
+    * @return a copy of the Card object at cards[k]
+    */
    public Card inspectCard(int k)
    {
       Card cardToInspect;
@@ -210,12 +265,21 @@ class Deck
    }
 
 
+   /**
+    * Functions similarly to inspectCard, but only inspects the current topCard.
+    * 
+    * @return a copy of the topCard; the Card object at cards[0]
+    */
    public Card getTopCard()
    {
       return new Card(cards[topCard]);
    }
 
 
+   
+   
+   
+   
    /**
     * The following function converts int values to a Card.Suit enum.
     * For use with quick access of enums.
@@ -228,19 +292,24 @@ class Deck
    {
       switch (toBeConverted)
       {
-      case 0:
-         return Card.Suit.hearts;
-      case 1:
-         return Card.Suit.clubs;
-      case 2:
-         return Card.Suit.diamonds;
-      default:
-         return Card.Suit.spades;
+         case 0:
+            return Card.Suit.hearts;
+         case 1:
+            return Card.Suit.clubs;
+         case 2:
+            return Card.Suit.diamonds;
+         default:
+            return Card.Suit.spades;
       }
 
    }
 
 
+   /**
+    * Handles creation of the masterPack.  Called by init and indirectly by constructors.
+    * Note that this only occurs once per object lifecycle. Repeated calls from the same object
+    * will have no effect.
+    */
    private static void allocateMasterPack()
    {
       if (masterPack[0] == null)
@@ -254,31 +323,31 @@ class Deck
                char cardValue = (char)('0' + cardNum);
                switch (cardNum)
                {
-               case 1:
-               {
-                  cardValue = 'A';
-                  break;
-               }
-               case 10:
-               {
-                  cardValue = 'T';
-                  break;
-               }
-               case 11:
-               {
-                  cardValue = 'J';
-                  break;
-               }
-               case 12:
-               {
-                  cardValue = 'Q';
-                  break;
-               }
-               case 13:
-               {
-                  cardValue = 'K';
-                  break;
-               }
+                  case 1:
+                  {
+                     cardValue = 'A';
+                     break;
+                  }
+                  case 10:
+                  {
+                     cardValue = 'T';
+                     break;
+                  }
+                  case 11:
+                  {
+                     cardValue = 'J';
+                     break;
+                  }
+                  case 12:
+                  {
+                     cardValue = 'Q';
+                     break;
+                  }
+                  case 13:
+                  {
+                     cardValue = 'K';
+                     break;
+                  }
                }
                masterPack[masterIndex] = new Card(cardValue, intToSuit(suitInt));
                ++masterIndex;
@@ -294,12 +363,18 @@ class Deck
 
 }
 
+
+
+
+
+
+
 class Hand
 {
-      
- //Keeps track the numbers of cards
-   private static int numCards; 
-   public static int MAX_CARDS = 100; 
+
+   //Keeps track the numbers of cards
+   private static int numCards;
+   public static int MAX_CARDS = 100;
    private static Card[] myCards = new Card[MAX_CARDS];
 
    //default constructor
@@ -307,13 +382,13 @@ class Hand
    {
       Hand.myCards= new Card[MAX_CARDS];
    }
-   
-   //remove all cards from the hand 
+
+   //remove all cards from the hand
    public void resetHand() {
-      
+
       myCards = new Card[MAX_CARDS];
       numCards = 0;
-      
+
    }
 
    //Helper function to take a newCard from the table and add to myArray
@@ -321,12 +396,12 @@ class Hand
    {
       myCards[numCards] = newCard;
       ++numCards;
-      
+
       return true;
    }
 
    //Remove card from hand and return that card
-   
+
    public Card playCard()
    {
       --numCards;
@@ -334,10 +409,10 @@ class Hand
       myCards[numCards] = null;
       return cardDrawn;
    }
-   
+
    //Used prior to displaying the entire hand.
    public String toString() {
-      
+
       String cards = "";
       for(int i = 0; i<numCards; i++) {
          cards += myCards[i].getValue() + " of " + myCards[i].getSuit() + ", ";
@@ -345,19 +420,19 @@ class Hand
       return "Hand = (" + cards + ") ";
    }
    //Accessor for numCards.
-   
+
    public int getnumCards() {
-      
+
       return numCards;
    }
-      
+
    //Accessor for an individual card and returns a card with errorFalg if K is not valid.
-   
+
    public Card inspectCard(int k) {
-      
-      if (k<=MAX_CARDS && k>=0){ 
-         return myCards[k];             
-      }   
+
+      if (k<=MAX_CARDS && k>=0){
+         return myCards[k];
+      }
       return new Card('T', Card.Suit.hearts);
    }
 
@@ -378,46 +453,46 @@ public class Main
 
    public static void main(String[] args)
    {
-      
-      
+
+
       if(TESTING)
       {
-      //set up test of card class
-      testCards[0] = new Card('Q', Card.Suit.hearts);
-      testCards[1] = new Card('D', Card.Suit.hearts);
-      testCards[2] = new Card();
-      //print test cards
-      for(Card card : testCards)
-         System.out.println(card.toString());
-      //fix the 2 bad ones
-      testCards[1].setValue('6');
-      testCards[2].setValue('F');
-      System.out.println();
-      //print fixed test cards
-      for(Card card : testCards)
-         System.out.println(card);
-      //End of Card class Testing
+         //set up test of card class
+         testCards[0] = new Card('Q', Card.Suit.hearts);
+         testCards[1] = new Card('D', Card.Suit.hearts);
+         testCards[2] = new Card();
+         //print test cards
+         for(Card card : testCards)
+            System.out.println(card.toString());
+         //fix the 2 bad ones
+         testCards[1].setValue('6');
+         testCards[2].setValue('F');
+         System.out.println();
+         //print fixed test cards
+         for(Card card : testCards)
+            System.out.println(card);
+         //End of Card class Testing
       }
-      
+
       //Testing hand class
       System.out.println("");
       System.out.println("**Testing the hand class**");
       Hand myHand = new Hand();
-        
+
       myHand.takeCard(new Card ('A', Card.Suit.hearts));
       myHand.takeCard(new Card ('Q', Card.Suit.clubs));
       myHand.takeCard(new Card ('9', Card.Suit.spades));
-      myHand.takeCard(new Card ('2', Card.Suit.diamonds));   
+      myHand.takeCard(new Card ('2', Card.Suit.diamonds));
       myHand.takeCard(new Card ('A', Card.Suit.hearts));
       myHand.takeCard(new Card ('Q', Card.Suit.clubs));
       myHand.takeCard(new Card ('9', Card.Suit.spades));
-      myHand.takeCard(new Card ('2', Card.Suit.diamonds));  
+      myHand.takeCard(new Card ('2', Card.Suit.diamonds));
       myHand.takeCard(new Card ('A', Card.Suit.hearts));
       myHand.takeCard(new Card ('Q', Card.Suit.clubs));
       myHand.takeCard(new Card ('9', Card.Suit.spades));
-      myHand.takeCard(new Card ('2', Card.Suit.diamonds));  
-      
-      
+      myHand.takeCard(new Card ('2', Card.Suit.diamonds));
+
+
       System.out.println(myHand.toString());
       System.out.println("");
       System.out.println("Testing inspectCard()");
@@ -428,55 +503,55 @@ public class Main
       System.out.println("** illegal **");
       System.out.println(myHand.inspectCard(200));
       System.out.println(myHand.inspectCard(-1));
-      
+
       while(myHand.getnumCards()!= 0) {
          System.out.println("playing " + myHand.playCard());
-      }      
+      }
       myHand.resetHand();
       System.out.println("");
       System.out.println("After playing all cards");
       System.out.println(myHand.toString());
       System.out.println("");
 
-      
+
       System.out.println("####################" + "\n" +
-    		  			 "### Deck Testing ###" + "\n"+
-    		             "####################" + "\n\n");
-      
-      
+         "### Deck Testing ###" + "\n"+
+         "####################" + "\n\n");
+
+
       Deck testDeckOnePack = new Deck();
       for (int i = 0; i < 52; ++i)
       {
-    	  System.out.println(testDeckOnePack.dealCard());
+         System.out.println(testDeckOnePack.dealCard());
       }
       testDeckOnePack.shuffle();
       for (int i = 0; i < 52; ++i)
       {
-    	  System.out.println(testDeckOnePack.dealCard());
+         System.out.println(testDeckOnePack.dealCard());
       }
-      
+
       Deck testDeckSixPacks = new Deck(6);
       for (int i = 0; i < 6*52; ++i)
       {
-    	  System.out.println(testDeckSixPacks.dealCard());
+         System.out.println(testDeckSixPacks.dealCard());
       }
       testDeckSixPacks.shuffle();
       for (int i = 0; i < 6*52; ++i)
       {
-    	  System.out.println(testDeckSixPacks.dealCard());
+         System.out.println(testDeckSixPacks.dealCard());
       }
 
-      
+
       testDeckOnePack.shuffle();
       System.out.println(testDeckOnePack.inspectCard(2));
       System.out.println(testDeckOnePack.getTopCard());
       System.out.println(testDeckOnePack.inspectCard(2));
       System.out.println(testDeckOnePack.getTopCard());
-      
-      
+
+
       /*
        * Attempt to throw an error by creating a set larger than Deck.MAX_CARDS
-       
+
       Deck testDeckTooManyPacks = new Deck(7);
       for (int i = 0; i < 7*52; ++i)
       {
