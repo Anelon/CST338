a929737902bf798a65b8088b334a5587603a009b
Andrew <48335990+espresso64@users.noreply.github.com>
2019-05-28 21:18:56 +0800

    changed interface to be public

--- Assig4/src/Main.java
+++ Assig4/src/Main.java
@@ -2,14 +2,14 @@
 
 interface BarcodeIO
 {
-   boolean scan(BarcodeImage bc);
+   public boolean scan(BarcodeImage bc);
 
-   boolean readText(String text);
+   public boolean readText(String text);
 
-   boolean generateImageFromText();
-   boolean translateImageToText();
-   void displayTextToConsole();
-   void displayImageToConsole();
+   public boolean generateImageFromText();
+   public boolean translateImageToText();
+   public void displayTextToConsole();
+   public void displayImageToConsole();
 }
 
 
