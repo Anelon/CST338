ba716409d2f8f3836e133a62449873f4250a79d7
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2019-05-30 10:45:37 -0700

    Stubs set up, old code pulled in

--- Assig5/src/phase2.java
+++ Assig5/src/phase2.java
@@ -1,1 +1,559 @@
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.text.DecimalFormat;
+import java.util.Random;
+import java.lang.IllegalArgumentException;
+
+
+public class Main
+{
+
+
+   // a simple main to throw all the JLabels out there for the world to see
+   public static void main(String[] args)
+   {
+
+   }
+}
+
+
+class GUICard
+{
+   private static Icon[][] iconCards = new ImageIcon[14][4]; // 14 = A thru K + joker
+   private static Icon iconBack;
+   static boolean iconsLoaded = false;
+
+   static void loadCardIcons()
+   {
+
+   }
+
+   public static Icon getIcon(Card card)
+   {
+
+   }
+
+   public static Icon getBackCardIcon()
+   {
+
+   }
+
+}
+
+
+
+class CardTable extends JFrame
+{
+   static int MAX_CARDS_PER_HAND = 56;
+   static int MAX_PLAYERS = 2;  // for now, we only allow 2 person games
+
+   private int numCardsPerHand;
+   private int numPlayers;
+
+   public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
+
+   CardTable(String title, int numCardsPerHand, int numPlayers)
+   {
+
+   }
+
+   public int getNumCardsPerHand() {
+
+   }
+
+   public int getNumPlayers()
+   {
+
+   }
+}
+
+class Card
+{
+   public enum Suit {spades, hearts, clubs, diamonds};
+   //Characters to hold the values cards can be
+   private static final char ACE = 'A', KING = 'K', QUEEN = 'Q', JACK = 'J';
+   private static final char TEN = 'T';
+   private static final char MIN_VALUE = '2', MAX_VALUE = '9';
+
+   //private class values of suit, card value, and errorflag
+   private Suit suit;
+   private char value;
+   //holds if an error has occured
+   private boolean errorFlag = false;
+
+   //Static finals for default card values
+   public static final Suit DEFAULT_SUIT = Suit.spades;
+   public static final char DEFAULT_VALUE = ACE;
+
+   //card to string converter
+   public String toString()
+   {
+      if(errorFlag)
+         return "[Invalid]";
+      return value + " of " + suit;
+
+   }
+
+   //returns if this card and another card are the same
+   public boolean equals(Card card)
+   {
+      return card.getValue() == value && card.getSuit() == suit;
+   }
+
+   //default constructor
+   public Card()
+   {
+      value = ACE;
+      suit = Suit.spades;
+   }
+
+   //For use of checking of the values are valid
+   private boolean isValid(char newValue, Suit newSuit)
+   {
+      if ((newValue >= MIN_VALUE && newValue <= MAX_VALUE)
+         || newValue == ACE || newValue == KING || newValue == QUEEN
+         || newValue == TEN || newValue == JACK)
+      {
+         return true;
+      }
+      return false;
+   }
+
+   //get Number for the card returns if its valid and value was set
+   public boolean setValue(char newValue)
+   {
+      if (isValid(newValue, suit))
+      {
+         value = newValue;
+         errorFlag = false;
+         return true;
+      }
+      errorFlag = true;
+      return false;
+   }
+
+   //takes in a new suit and if its valid sets the suit and returns true
+   public boolean setSuit(Suit newSuit)
+   {
+      suit = newSuit;
+      return true;
+   }
+
+   //takes both values and sets them
+   public boolean set(char newValue, Suit newSuit)
+   {
+      setSuit(newSuit);
+      return setValue(newValue);
+   }
+
+   //returns the value of the card as a char
+   public char getValue()
+   {
+      return value;
+   }
+
+   //returns the suit of the card as an enum
+   public Suit getSuit()
+   {
+      return suit;
+   }
+
+   //returns if an error has occured
+   public boolean getErrorFlag()
+   {
+      return errorFlag;
+   }
+
+   //collection of constructors
+   public Card(char newValue, Suit newSuit)
+   {
+      errorFlag = !setValue(newValue);
+      setSuit(newSuit);
+   }
+   public Card(Suit newSuit)
+   {
+      this(ACE, newSuit);
+   }
+   public Card(char newValue)
+   {
+      this(newValue, Suit.spades);
+   }
+   //constructor for duplicating cards
+   public Card(Card copy)
+   {
+      errorFlag = !setValue(copy.getValue());
+      setSuit(copy.getSuit());
+
+   }
+}
+
+
+
+
+
+
+/**
+ * Class containing data for a deck of cards.  Contains static
+ * information of all possible cards as well as current in-use
+ * cards/order within the Deck object.
+ *
+ * public static int MAX_CARDS - Maximum number of cards allowed in play at
+ *    once.
+ * private Card[] cards - current "in use" cards for playing
+ * private int topCard - top card of the deck, next to be "dealt".
+ * private static Card[] masterPack - static representation of all possible
+ *    cards
+ */
+class Deck
+{
+
+
+   private Card[] cards;
+   private int topCard;
+   public final int MAX_CARDS = 6*52;
+   private static Card[] masterPack = new Card[52];
+
+
+
+
+
+
+   /**
+    * Constructor that creates the deck based on the number of "packs" given.
+    * Note that only a maximum of MAX_CARDS cards can be used.
+    *
+    * @param numPacks - number of traditional 52 card packs to be used in
+    *                 deck creation
+    */
+   public Deck(int numPacks)
+   {
+
+      allocateMasterPack();
+
+      if (52*numPacks > MAX_CARDS)
+         cards = new Card[MAX_CARDS];
+      else
+         cards = new Card[52*numPacks];
+
+      init(numPacks);
+   }
+
+
+
+
+
+
+   /**
+    * Empty constructor. Initializes the deck with a default of a single
+    * 52 card pack.
+    */
+   public Deck()
+   {
+      this(1);
+   }
+
+
+
+
+
+
+   /**
+    * initializes the playable cards in cards[] based on the number of packs.
+    * The resulting cards[] object will contain ((int)numPacks) copies of the
+    * masterPack.
+    * Called by the constructors.
+    *
+    * @param numPacks number of 52 card packs to be used
+    */
+   public void init(int numPacks)
+   {
+      topCard = 0;
+      int totalCardIndex = 0;
+      for (int packNumber = 0; packNumber < numPacks; ++packNumber)
+      {
+         for (int masterIndex = 0; masterIndex < masterPack.length;
+              ++masterIndex)
+         {
+            cards[totalCardIndex] = new Card(masterPack[masterIndex]);
+            ++totalCardIndex;
+         }
+      }
+   }
+
+
+
+
+
+
+   /**
+    * Randomizes the cards within the in-use cards[] object and resets
+    * the topCard to the first position.
+    */
+   public void shuffle()
+   {
+      Random randomGenerator = new Random();
+      int randomIndex = randomGenerator.nextInt(cards.length);
+
+      for (int i = 0; i < cards.length; ++i)
+      {
+         Card placeholder = new Card(cards[randomIndex]);
+         cards[randomIndex] = cards[i];
+         cards[i] = placeholder;
+         randomIndex = randomGenerator.nextInt(cards.length);
+      }
+
+      topCard = 0;
+   }
+
+
+   /**
+    * "Deals" the top card from the deck. The next card in cards[] becomes
+    * the new topCard.
+    *
+    * @return the topCard at the time of calling
+    */
+   public Card dealCard()
+   {
+      return new Card(cards[topCard++]);
+   }
+
+
+   /**
+    * Gives information from the card at the given index within cards[].
+    * Note that the card at the index is not returned, only a copy in order
+    * to prevent the card itself from being altered in any way.
+    *
+    * @param k - index of cards[] to be checked
+    * @return a copy of the Card object at cards[k]
+    */
+   public Card inspectCard(int k)
+   {
+      Card cardToInspect;
+
+      if (k < cards.length && k >= 0)
+      {
+         cardToInspect = new Card(cards[k]);
+      }
+      else
+         cardToInspect  = new Card('0', Card.Suit.spades);
+
+      return cardToInspect;
+   }
+
+
+   /**
+    * Functions similarly to inspectCard, but only inspects the current
+    * topCard.
+    *
+    * @return a copy of the topCard; the Card object at cards[0]
+    */
+   public Card getTopCard()
+   {
+      return new Card(cards[topCard]);
+   }
+
+
+
+
+
+
+   /**
+    * The following function converts int values to a Card.Suit enum.
+    * For use with quick access of enums.
+    *
+    * @param toBeConverted - integer to be converted to a Card.Suit enum
+    * @return one of the 4 suits as defined in Card class. 0 = hearts,
+    * 1 = clubs, 2 = diamonds, all other ints will return spades
+    */
+   private static Card.Suit intToSuit(int toBeConverted)
+   {
+      switch (toBeConverted)
+      {
+         case 0:
+            return Card.Suit.hearts;
+         case 1:
+            return Card.Suit.clubs;
+         case 2:
+            return Card.Suit.diamonds;
+         default:
+            return Card.Suit.spades;
+      }
+
+   }
+
+
+   /**
+    * Handles creation of the masterPack.  Called by init and
+    * indirectly by constructors.
+    * Note that this only occurs once per object lifecycle.
+    * Repeated calls from the same object
+    * will have no effect.
+    */
+   private static void allocateMasterPack()
+   {
+      if (masterPack[0] == null)
+      {
+         int masterIndex = 0;
+
+         for(int suitInt = 0; suitInt < 4; ++suitInt)
+         {
+            for (int cardNum = 1; cardNum <=13; ++cardNum)
+            {
+               char cardValue = (char)('0' + cardNum);
+               switch (cardNum)
+               {
+                  case 1:
+                  {
+                     cardValue = 'A';
+                     break;
+                  }
+                  case 10:
+                  {
+                     cardValue = 'T';
+                     break;
+                  }
+                  case 11:
+                  {
+                     cardValue = 'J';
+                     break;
+                  }
+                  case 12:
+                  {
+                     cardValue = 'Q';
+                     break;
+                  }
+                  case 13:
+                  {
+                     cardValue = 'K';
+                     break;
+                  }
+               }
+
+               masterPack[masterIndex] =
+                  new Card(cardValue, intToSuit(suitInt));
+               ++masterIndex;
+            }
+         }
+      }
+   }
+
+
+
+
+
+
+   /**
+    * Tests deck init, shuffle, and dealCard
+    */
+   public void testDeck(int numPacks)
+   {
+      init(numPacks);
+
+      for (int currentCard = 0; currentCard < cards.length; ++ currentCard)
+      {
+         System.out.println(dealCard());
+      }
+
+      init(numPacks);
+      shuffle();
+
+
+      for (int currentCard = 0; currentCard < cards.length; ++ currentCard)
+      {
+         System.out.println(dealCard());
+      }
+   }
+}
+
+
+
+
+
+
+/*This is the hand class. It holds data for the player's hand.*/
+class Hand
+{
+
+   /*Keeps track the numbers of cards. */
+   private int numCards;
+   public static int MAX_CARDS = 100;
+   private Card[] myCards = new Card[MAX_CARDS];
+
+   /*Default constructor for the hand class.*/
+   public Hand()
+   {
+      this.myCards= new Card[MAX_CARDS];
+   }
+
+   /*This function remove all cards from the hand. */
+   public void resetHand() {
+
+      myCards = new Card[MAX_CARDS];
+      numCards = 0;
+
+   }
+
+   /*This is a helper function to take a newCard
+    * from the table and add to myCards array if
+    * there is room in the hand.*/
+   public boolean takeCard(Card newCard)
+   {
+      if(numCards >=MAX_CARDS) {
+         return false;
+      }
+
+      myCards[numCards] = new Card(newCard.getValue(), newCard.getSuit());
+      numCards++;
+
+      return true;
+   }
+
+   /*This function remove card from hand and
+    *  return that card. */
+
+   public Card playCard()
+   {
+      --numCards;
+      Card cardDrawn = myCards[numCards];
+      myCards[numCards] = null;
+      return cardDrawn;
+   }
+
+   /*This function is used prior to displaying
+    *the entire hand.*/
+   public String toString() {
+
+      String cards = "";
+      for(int i = 0; i<numCards; i++) {
+         cards += myCards[i].getValue() + " of " + myCards[i].getSuit() + ", ";
+      }
+      return "Hand = (" + cards + ") ";
+   }
+   /* This is the accessor method for numCards. */
+
+   public int getnumCards() {
+
+      return numCards;
+   }
+
+   /* This is accessor for an individual card and
+    * it returns a card with errorFalg if K is not valid.*/
+
+   public Card inspectCard(int k) {
+
+      if (k<=MAX_CARDS && k>=0){
+         return myCards[k];
+      }
+      return new Card('T', Card.Suit.hearts);
+   }
+
+
+}
+
+
+
+
